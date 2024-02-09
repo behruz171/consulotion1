348c66a0e832c404cd18602516be523cbf773fdd
behruz171 <behruzzo662@gmail.com>
2024-02-09 15:53:39 +0500

    nimadir

--- cor/settings.py
+++ cor/settings.py
@@ -116,6 +116,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/var/www/static/",
 ]
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
